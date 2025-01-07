4b5eea69fde6e9eef8c1e079b3a579beb5eaffab
Andrew Ofuenweuche <chidi.andrew@gmail.com>
2025-01-07 02:18:54 +0100

    server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,7 @@ app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
 
 // SERVER
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
